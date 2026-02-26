c6a74ecb767643c154462665254074125d9221bf
Danek <danek@gmail.com>
2026-02-26 13:48:14 +0300

    Added author info button and window

---
+++ Group4338/4338_Kirillovykh.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace Group4338
+{
+    public partial class _4338_Kirillovykh : Window
+    {
+        public _4335_Ivanov()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Group4338/MainWindow.xaml.cs
+++ Group4338/MainWindow.xaml.cs
@@ -7,4 +7,10 @@ namespace Group4338
         public MainWindow()
             => InitializeComponent();
     }
-}
+    private void AuthorInfoButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            var authorWindow = new _4338_Kirillovykh(); 
+            authorWindow.ShowDialog();
+        }
+    }
